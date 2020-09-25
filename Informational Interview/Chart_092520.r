4d52e5af84675e7b3713bcba385c1366b6b9aac6
Min Hwangbo <37720105+mhwangbo11@users.noreply.github.com>
2020-09-25 13:21:03 -0700

    Update Chart_092520.r

--- Informational Interview/Chart_092520.r
+++ Informational Interview/Chart_092520.r
@@ -1,5 +1,10 @@
 Informational Interview
 
+Lamar F.
+
 * What is the metrics that you'd like to gain from doing these data science piece?
 * Having that domain language of R, Python, or SQL... within the educational sectors to re-imagine how education could be?
 * Standford launched data science and education.
+* Policy and documents just gets thrown in and people are trying to make sense of it to see what are the big takeaways?
+* Long term growth of students and trying to see what do we see with trajectories with certain populations? 
+* Every school district would be beneficial to have a data science team with the support or facet of research team.
