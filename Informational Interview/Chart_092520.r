1f5c59f51738857f9c003480f3438870fe720151
Min Hwangbo <37720105+mhwangbo11@users.noreply.github.com>
2020-09-25 13:18:52 -0700

    Create Chart_092520.r

---
+++ Informational Interview/Chart_092520.r
@@ -0,0 +1,4 @@
+Informational Interview
+* What is the metrics that you'd like to gain from doing these data science piece?
+* Having that domain language of R, Python, or SQL... within the educational sectors to re-imagine how education could be?
+* Standford launched data science and education 
