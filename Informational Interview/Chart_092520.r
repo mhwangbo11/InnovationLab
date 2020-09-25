97da43c2d06cdc45368a4b406e89d397d7cc0002
Min Hwangbo <37720105+mhwangbo11@users.noreply.github.com>
2020-09-25 13:22:16 -0700

    Update Chart_092520.r

--- Informational Interview/Chart_092520.r
+++ Informational Interview/Chart_092520.r
@@ -8,3 +8,4 @@ Lamar F.
 * Policy and documents just gets thrown in and people are trying to make sense of it to see what are the big takeaways?
 * Long term growth of students and trying to see what do we see with trajectories with certain populations? 
 * Every school district would be beneficial to have a data science team with the support or facet of research team.
+* How do we create a lane of data science in education?
